1f0c91f8e1cebd3c5d7ca204ef03ad3a6d1d4827
super <lexmengual@gmail.com>
2019-10-25 20:11:25 +0200

    Migrate

--- vr/models.py
+++ vr/models.py
@@ -2,26 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Room(models.Model):
-    id_room = models.AutoField(primary_key=True)
+    class Meta:
+        verbose_name_plural = "Room"
     capacity = models.IntegerField()
     location = models.CharField(max_length=200)
     glasses_availables = models.IntegerField();
 
 class Glasses(models.Model):
-    id_glasses = models.AutoField(primary_key=True)
-    id_room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = "Glasses"
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     glasses_model = models.CharField(max_length=200)
 
 class Reservation(models.Model):
-    id_reservation = models.AutoField(primary_key=True)
-    id_person = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = "Reservation"
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    glasses = models.ManyToManyField(Glasses)
     day = models.DateField()
     init_hour = models.DateTimeField()
     final_hour = models.DateTimeField()
     num_persons = models.IntegerField()
-
-class ReservationGlass(models.Model):
-    id_reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
-    id_glasses = models.ForeignKey(Glasses, on_delete=models.CASCADE)
-    stock = models.IntegerField()
